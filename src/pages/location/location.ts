38a9115185a5c217f2130d723c623e80b13f9b33
danielerian2 <danielerian2@gmail.com>
2017-06-23 21:14:24 +0200

    Update location.ts port number
    from 3006 to 3000

--- src/pages/location/location.ts
+++ src/pages/location/location.ts
@@ -36,7 +36,7 @@ export class LocationPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http,
               private uniqueDeviceID: UniqueDeviceID) {
-                this.url = 'http://207.154.240.16:3006/';
+                this.url = 'http://207.154.240.16:3000/';
                 this.uniqueDeviceID.get()
               .then((uuid: any) => this.deviceID = uuid)
               .catch((error: any) => console.log(error));
