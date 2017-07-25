b453f923ab7d175c8bfa87eb89343c689d065c5f
danielerian2 <danielerian2@gmail.com>
2017-07-26 01:57:36 +0200

    search fully working

--- src/pages/search-test/search-test.ts
+++ src/pages/search-test/search-test.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Http, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/map';
+
 /**
  * Generated class for the SearchTestPage page.
  *
@@ -16,20 +19,18 @@ export class SearchTestPage {
 
   public showList: boolean;
 
-  orderTest= {
+  orderTest= {  //change default data according to mahmoud, or figure out a better way of doing deault ad empty values
 
     data: [
 
-    {id:1, name:"Medicine1", price:4, qty:3},
+    {id:-1, name:"", description:"", category:"", barcode:-1, milligrams:0, price:0},
 
-    {id:2, name:"Medicine2", price:5, qty:5},
-
-    {id:3, name:"Medicine3", price:2, qty:1},
 
   ]};
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  url:string;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
       this.showList = false;  //initialise things to be visible
+      this.url = 'http://146.185.148.66:3003/';
   }
 
   ionViewDidLoad() {
@@ -48,7 +49,20 @@ export class SearchTestPage {
 
     this.showList = true; //when start searching, show the list
     //if statement make it false when deleted all characters!!
+    //break if backspace and emtpy 3shan el requests
 
+    this.http.get(this.url + 'search/' + ev.target.value.toString(), new RequestOptions({headers: this.setGetHeaders()}))
+      .map(res => res).subscribe(data => {
+      console.log(this.url + 'search/' + ev.target.value.toString());
+
+        //this.orderTest["data"] = JSON.parse(data["_body"].toString());
+        if(data["_body"].toString() == "No results found"){
+          this.orderTest["data"] = [{id:-1, name:"No results found", description:"", category:"", barcode:-1, milligrams:0, price:0}]; //empty ot message
+        }else{
+          this.orderTest["data"] = JSON.parse(data["_body"].toString());
+        }
+        console.log(data);
+      });
 
     console.log(ev.target.value);
     console.log(this.orderTest["data"][0]["name"]);
@@ -58,4 +72,14 @@ export class SearchTestPage {
     this.showList = false; //and true to show other components
   }
 
+
+  setGetHeaders(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*');
+    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+    headers.append('Authorization', 'Basic YWRtaW46MTIzNDU2');
+
+    return headers;
+  }
 }
